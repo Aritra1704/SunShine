0f416d7d4084526c722bcfa232bd11f60fc3951c
Aritra1704 <aritra1704@gmail.com>
2016-04-04 11:38:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/arpaul/sunshine/SunShineActivity.java
@@ -0,0 +1,13 @@
+package com.example.arpaul.sunshine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SunShineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sun_shine);
+    }
+}
